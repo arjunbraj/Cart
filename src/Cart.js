1e2ea07d44b3218e2632ced403b2a788619f55e1
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-03-24 12:40:25 +0530

    added decreaseQuantity func

--- src/Cart.js
+++ src/Cart.js
@@ -41,6 +41,19 @@ class Cart extends React.Component{
         });
     }
 
+    handleDecreaseQuantity = (product) => {
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        if(products[index].qty === 0){
+            return;
+        }
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        });
+    }
+
     render(){
         const {products} = this.state;
         return (
@@ -51,6 +64,7 @@ class Cart extends React.Component{
                             product={product} 
                             key={product.id}
                             onIncreaseQuantity={this.handleIncreaseQuantity} 
+                            onDecreaseQuantity={this.handleDecreaseQuantity}
                         />
                     )
                 })}
