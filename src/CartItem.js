2a85dad5dde0a23cedf46090d6bf619769862b0d
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-03-24 13:39:27 +0530

    converted cartItem to functional component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,41 +1,40 @@
 import React from "react";
 
-class CartItem extends React.Component{
+const CartItem = (props) => {
 
-    render(){
-        const {price, title, qty} = this.props.product;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image} alt = ''/>
-                </div>
-                <div className="right-block">
-                    <div style={{fontSize: 25}}>{title}</div>
-                    <div style={{color: '#777'}}>Price: Rs {price}</div>
-                    <div style={{color: '#777'}}>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                        <img 
-                        alt="increase" 
-                        className="action-icons" 
-                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
-                        onClick={() => this.props.onIncreaseQuantity(this.props.product)} />
-                        <img 
-                        alt="decrease" 
-                        className="action-icons" 
-                        src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
-                        onClick={() => this.props.onDecreaseQuantity(this.props.product)} />
-                        <img 
-                        alt="delete" 
-                        className="action-icons" 
+    const { price, title, qty } = props.product;
+    return (
+        <div className="cart-item">
+            <div className="left-block">
+                <img style={styles.image} alt='' />
+            </div>
+            <div className="right-block">
+                <div style={{ fontSize: 25 }}>{title}</div>
+                <div style={{ color: '#777' }}>Price: Rs {price}</div>
+                <div style={{ color: '#777' }}>Qty: {qty}</div>
+                <div className="cart-item-actions">
+                    {/* Buttons */}
+                    <img
+                        alt="increase"
+                        className="action-icons"
+                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                        onClick={() => props.onIncreaseQuantity(props.product)} />
+                    <img
+                        alt="decrease"
+                        className="action-icons"
+                        src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                        onClick={() => props.onDecreaseQuantity(props.product)} />
+                    <img
+                        alt="delete"
+                        className="action-icons"
                         src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"
-                        onClick={() => this.props.onDeleteItem(this.props.product.id)} />
-                    </div>
+                        onClick={() => props.onDeleteItem(props.product.id)} />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 const styles = {
     image:{
